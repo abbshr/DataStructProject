7da5c6d345191df30edfee2539d66ff2d83d714f
abbshr <abbshr@outlook.com>
2013-12-02 18:16:51 +0800

    ~

--- main.c
+++ main.c
@@ -1,10 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#include <alogthm.h>
+#include <sys/time.h>
+
+/*strut Timeval
+{
+    long tv_sec; 
+    long tv_usec; 
+
+};*/
 
 int main(int argc, char const *argv[])
 {
+    timeval startTime, endTime;
+    double timeuse;
 	/* read from file */
 	char* str = NULL;
 	int n;
@@ -17,15 +26,32 @@ int main(int argc, char const *argv[])
 	printf("输入模式串：\n");
 	scanf("%c", p);
 
-	/* comp */
-	Naive_Match(p, str, n, m);
+    gettimeofday(&startTime, 0);
+	   Naive_Match(p, str, n, m);
+    gettimeofday(&endTime,0);
+    timeuse = 1000000 * (endtime.tv_sec - starttime.tv_sec) 
+            + endtime.tv_usec - startime.tv_usec;
+    timeuse /= 1000;  //除以1000则进行毫秒计时，如果除以1000000则进行秒级别计时，如果除以1则进行微妙级别计时
+    printf("%f\n", timeuse);
+	
+    gettimeofday(&startTime, 0);
+        Rabin_Karp();
+    gettimeofday(&endTime,0);
+    timeuse = 1000000 * (endtime.tv_sec - starttime.tv_sec) 
+            + endtime.tv_usec - startime.tv_usec;
+    timeuse /= 1000;  //除以1000则进行毫秒计时，如果除以1000000则进行秒级别计时，如果除以1则进行微妙级别计时
+    printf("%f\n", timeuse);
 
-	Rabin_Karp();
-
+    gettimeofday(&startTime, 0);
 	int* next = NULL;
 	next = (int* )malloc(sizeof(int) * m);
 	preprocess(next, p, m);
-	KMP(next, p, str, n, m);
+	   KMP(next, p, str, n, m);
+    gettimeofday(&endTime,0);
+    timeuse = 1000000 * (endtime.tv_sec - starttime.tv_sec) 
+            + endtime.tv_usec - startime.tv_usec;
+    timeuse /= 1000;  //除以1000则进行毫秒计时，如果除以1000000则进行秒级别计时，如果除以1则进行微妙级别计时
+    printf("%f\n", timeuse);
 
 	return 0;
-}
+}　
